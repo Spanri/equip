18c419645810955f8c06801ca2e0c1b0e2d82808
Spanri <kozlova9v@mail.ru>
2018-07-12 09:35:32 +0300

    add ipython

--- index.py
+++ index.py
@@ -1,3 +1,11 @@
+#%% Test11
+"""
+This will be another comment line 
+for the testing of the different sections
+"""
+from notebook.auth import passwd
+passwd('nysha2161')
+
 import sys
 import os
 sys.path.append(os.path.join(sys.path[0], 'modules'))
@@ -13,7 +21,8 @@ ser = processing.version(resPd)
 ser = processing.smth(resPd, ser)
 
 resPd = connect.series(resPd, ser)
+print(resPd)
 
 connect.end(resPd, './excel/output4.xlsx')
-
+#%% Test12
 print('Normas')
